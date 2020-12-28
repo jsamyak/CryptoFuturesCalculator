a1a04c6a92d9246fafc0d82cbb9dca12abf872e5
Samyak Jain <iamsamyakjain@gmail.com>
2020-12-28 13:13:27 -0800

    Add net return percentage

--- app.R
+++ app.R
@@ -6,17 +6,24 @@ source("my_server.R")
 my_ui <- shinyUI(
   fluidPage(
     h1("Crypto Futures Calculator"),
+    
     numericInput("entry_price", "Entry Price", NA),
     numericInput("exit_price", "Exit Price", NA),
     numericInput("leverage", "Leverage", NA),
     numericInput("asset_quantity", "Asset Quantity", NA),
     numericInput("maintenance_margin_rate", "Maintenance Margin Rate", NA),
+    
     h4("Initial Margin Required", style = "text-align:left;color:black;
        background-color:lavender;padding:15px;border-radius:10px"),
     textOutput("initial_margin"),
+    
     h4("Net profit (without fees)", style = "text-align:left;color:black;
        background-color:lavender;padding:15px;border-radius:10px"),
-    textOutput("net_profit")
+    textOutput("net_profit"),
+    
+    h4("Net return percentage", style = "text-align:left;color:black;
+       background-color:lavender;padding:15px;border-radius:10px"),
+    textOutput("net_return_percentage")
   )
 )
 
--- my_server.R
+++ my_server.R
@@ -2,27 +2,35 @@ library(shiny)
 
 my_server <- function(input, output) {
   output$initial_margin <- renderText({
-    input$entry_price * input$asset_quantity / input$leverage
+    initialMarginCalculator(input$entry_price, input$asset_quantity, input$leverage)
   })
   
   output$net_profit <- renderText({
-    (input$exit_price - input$entry_price) * input$asset_quantity
+    netProfitCalculator(input$entry_price, input$exit_price, input$asset_quantity)
+  })
+  
+  output$net_return_percentage <- renderText({
+    netProfitCalculator(input$entry_price, input$exit_price, input$asset_quantity) /
+      initialMarginCalculator(input$entry_price, input$asset_quantity, input$leverage) *
+      100
   })
 }
 
+initialMarginCalculator <- function(entryPrice, assetQuantity, leverage) {
+  return (entryPrice * assetQuantity / leverage)
+}
+
+netProfitCalculator <- function(entryPrice, exitPrice, assetQuantity) {
+  return ((exitPrice - entryPrice) * assetQuantity)
+}
 # input parameters
 
 entry_price <- 100000
 exit_price <- 120
-
 leverage <- 20
-
 asset_quantity <- 1
-
 maintenance_margin_rate <- 0.02 #(https://www.binance.com/en/support/faq/360033162192 refer this website for rate brackets)
-
 #~~~~~~~~~~~~~~~
-
 #output parameters
 
 initial_margin <- entry_price * asset_quantity / leverage
