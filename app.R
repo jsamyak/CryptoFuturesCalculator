bf4e67d0eb2ad2f4df3b38399d4355953b2b820d
Samyak Jain <iamsamyakjain@gmail.com>
2020-12-28 12:46:14 -0800

    Transfer code from my_ui to app for hot reloading in development

--- app.R
+++ app.R
@@ -1,6 +1,19 @@
 library(shiny)
 
-source("my_ui.R")
+#source("my_ui.R")
 source("my_server.R")
+
+my_ui <- shinyUI(
+  fluidPage(
+    h1("Crypto Futures Calculator"),
+    numericInput("entry_price", "Entry Price", NA),
+    numericInput("exit_price", "Exit Price", NA),
+    numericInput("leverage", "Leverage", NA),
+    numericInput("asset_quantity", "Asset Quantity", NA),
+    numericInput("maintenance_margin_rate", "Maintenance Margin Rate", NA),
+    h4("Initial Margin Required", style = "text-align:left;color:black;background-color:lavender;padding:15px;border-radius:10px"),
+    textOutput("initial_margin")
+  )
+)
 
 shinyApp(ui = my_ui, server = my_server)
