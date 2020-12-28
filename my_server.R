995fc16c4bf5a85eaede97a28fe09527cd3471b3
Samyak Jain <iamsamyakjain@gmail.com>
2020-12-27 22:40:24 -0800

    Reconfig maintenance margin formula

--- my_server.R
+++ my_server.R
@@ -4,14 +4,14 @@ my_server <- function(input, output) {}
 
 # input parameters
 
-entry_price <- 11000
+entry_price <- 100000
 exit_price <- 120
 
 leverage <- 20
 
 asset_quantity <- 1
 
-maintenance_margin_rate <- 0.0065 #(https://www.binance.com/en/support/faq/360033162192 refer this website for rate brackets)
+maintenance_margin_rate <- 0.02 #(https://www.binance.com/en/support/faq/360033162192 refer this website for rate brackets)
 
 #~~~~~~~~~~~~~~~
 
@@ -23,9 +23,9 @@ net_profit <- (exit_price - entry_price) * asset_quantity
 
 net_return_percentage <- (net_profit / initial_margin) * 100
 
-# liquidation price includes other factors as well.
 initial_liquidation_price <- entry_price - (entry_price / leverage)
 
 maintenance_margin <- entry_price * maintenance_margin_rate
 
+# liquidation price includes other factors as well. The below price is slightly higher than actual
 final_liquidation_price <- initial_liquidation_price + maintenance_margin
