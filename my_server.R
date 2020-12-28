6dfa8e02a727a66805ab45f6fe98f6cb1ccd1210
Samyak Jain <iamsamyakjain@gmail.com>
2020-12-27 22:15:46 -0800

    Build formula for the Calculator

--- my_server.R
+++ my_server.R
@@ -1,3 +1,31 @@
 library(shiny)
 
 my_server <- function(input, output) {}
+
+# input parameters
+
+entry_price <- 11000
+exit_price <- 120
+
+leverage <- 20
+
+asset_quantity <- 1
+
+maintenance_margin_rate <- 0.0065 #(https://www.binance.com/en/support/faq/360033162192 refer this website for rate brackets)
+
+#~~~~~~~~~~~~~~~
+
+#output parameters
+
+initial_margin <- entry_price * asset_quantity / leverage
+
+net_profit <- (exit_price - entry_price) * asset_quantity
+
+net_return_percentage <- (net_profit / initial_margin) * 100
+
+# liquidation price includes other factors as well.
+initial_liquidation_price <- entry_price - (entry_price / leverage)
+
+maintenance_margin <- entry_price * maintenance_margin_rate
+
+final_liquidation_price <- initial_liquidation_price + maintenance_margin
