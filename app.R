4455d37d5e9e661c78ff380634ee298cdcef9671
Samyak Jain <iamsamyakjain@gmail.com>
2021-01-02 20:39:37 -0800

    Add heading

--- app.R
+++ app.R
@@ -158,6 +158,20 @@ my_ui <- shinyUI(
         hr(style = "border-top: 2px solid #000000;"),
         
         fluidRow(
+          column(1),
+          column(3,
+                 h4("Target (exit) price"),
+                 h4("without fee deduction")),
+          column(4),
+          column(3, 
+                 h4("Target (exit) price"),
+                 h4("with fee deduction")),
+          column(1),
+          style = "text-align:center;color:black;
+          background-color:lavender;border-radius:10px"
+        ),
+        
+        fluidRow(
           column(5,
                  tableOutput("return_exit_price")),
           column(2),
